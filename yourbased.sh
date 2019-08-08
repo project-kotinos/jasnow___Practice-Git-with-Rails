64e3e6248cf05d3fa85ebad93480223fa62b6f66
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 07:01:24 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,1 +1,14 @@
-
+#!/usr/bin/env bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y tzdata
+gem install bundler -v 2.0.1
+# before_install
+gem update --system
+gem update bundler
+# install
+bundle install --jobs=3 --retry=3
+# before_script
+bundle exec rake db:migrate RAILS_ENV=test
+# script
+rake
